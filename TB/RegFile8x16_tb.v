e6c52e06641a3aa0b42c1a261bc656076dc79a8c
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-07-31 00:56:28 +0300

    add a new tb for RegFile8x16.v after edition

--- TB/RegFile8x16_tb.v
+++ TB/RegFile8x16_tb.v
@@ -1,146 +1,160 @@
-`timescale 1us/1ns
-module RegFile8x16_tb ();
-//parameters
-parameter WIDTH = 8 ;
-parameter DEPTH = 16 ;
-parameter addressbits = 16 ;
-
-//declare tb signals 
-reg clk_tb ;
-reg rst_tb ;                          
-reg wrEN_tb ;                        
-reg rdEN_tb ;                       
-reg [addressbits-1:0] address_tb ; 
-reg [DEPTH-1:0] wrData_tb ;       
-wire[DEPTH-1:0] rdData_tb ;
-
-//DUT instantiation
-RegFile8x16 regfile (
-    .clk(clk_tb),
-    .rst(rst_tb),
-    .wrEN(wrEN_tb),
-    .rdEN(rdEN_tb),
-    .address(address_tb),
-    .wrData(wrData_tb),
-    .rdData(rdData_tb)
-);
-
-//clock generation 
-always #5 clk_tb = ~ clk_tb ;
-
-integer tests = 4 ; //! edit this if u added more tests
-integer succeeded = 0 ; 
-integer count ; //used for test2
-
-initial begin 
-    //intial values 
-    clk_tb = 0;
-    rst_tb = 1'b1;
-    wrEN_tb = 1'b0;
-    rdEN_tb = 1'b0;
-    address_tb = 3'b0;
-    wrData_tb = 'd5;
-    #2
-
-    //TESTING 
-    $display("Started TESTING\n");
-    
-    //TEST1 Write data and read from one reg
-        wrEN_tb = 1'b1;        //rise write enable
-        address_tb = 3'b000;  //REG0
-        wrData_tb = 'd5;     //write 5
-        #10
-        wrEN_tb = 1'b0;    //drop write enable 
-        #10
-        rdEN_tb = 1'b1;  //rise read enable
-        #10
-        if (rdData_tb == 'd5)begin
-            $display("TEST1 succeeded\n");
-            succeeded = succeeded + 1;
+`timescale 1ns/1ps
+module RegFile8x16_tb;
+    // DUT Parameters
+        parameter WIDTH = 8;
+        parameter DEPTH = 16;
+        parameter addressbits = 16; 
+    // Needed Parameters
+        parameter clk_period = 10;          // to maintain 50 MHZ clk frequency
+    // DUT Inputs
+        reg clk;
+        reg rst;                            //active low rst to reset all regs to 0
+        reg wrEN;                           //signal to write to a specified reg
+        reg [addressbits-1:0] address;      //address of the desired reg
+        reg [DEPTH-1:0] wrData;             //data to be writen to a reg
+    // Needed Signals
+        integer i;                          // needed for loops
+        integer succeeded_cases = 0;        // to count succeeded test cases
+        integer all_cases = DEPTH + 5;      // all test cases
+    // DUT Instantiation
+        RegFile8x16#(
+            .WIDTH(WIDTH), 
+            .DEPTH(DEPTH), 
+            .addressbits(addressbits)
+        )
+        DUT(
+            .clk(clk), 
+            .rst(rst), 
+            .wrEN(wrEN), 
+            .address(address), 
+            .wrData(wrData)
+        );
+    // CLK Generation
+        initial begin
+            clk = 0;        //initial value
+            forever #clk_period clk = ~clk;
         end 
-        else $display("TEST1 failed\n");
-        rdEN_tb =1'b0;//drop write
-        #10
+    // TASKs
+        // reset task
+            task reset(); begin
+                rst = 1;        // activate reset signal
+                wrEN = 0;       // default value
+                address = 0;    // default value
+                wrData = 0;     // default value
+                repeat(2)       // waits for 2 clk cycles to track rst signal
+                    @(negedge clk);
+                rst = 0;        // release reset signal
+            end
+            endtask
+    // Testing
+        initial begin
+            // 1st scenario : Functional Correctness (Reset Behavior)
+                $display("\n==================== 1st scenario : Functional Correctness (Reset Behavior) ====================");
+                reset();
+                for(i=0; i<DEPTH; i=i+1) begin
+                    if(DUT.REG[i] == 0) begin
+                        succeeded_cases = succeeded_cases + 1;  // another test case passes
+                        $display("[PASS] 1st scenario : Functional Correctness (Reset Behavior) | REG [%d] = %0h, expected = 0", i, DUT.REG[i]);
+                    end else begin
+                        $display("[FAIL] 1st scenario : Functional Correctness (Reset Behavior) | REG [%d] = %0h, expected = 0", i, DUT.REG[i]);
+                    end
+                end
 
-    //TEST2 Write data and read from multiple reg
-        //write REG1 13
-        count = 0 ;             //start counter
-        wrEN_tb = 1'b1;        //rise write enable
-        address_tb = 3'b001;  //REG1
-        wrData_tb = 'd13;    //write 13
-        #10
-        //write REG5 25
-        wrEN_tb = 1'b1;        //rise write enable
-        address_tb = 3'b101;  //REG5
-        wrData_tb = 'd25;    //write 25
-        #10
-        //write REG7 9
-        wrEN_tb = 1'b1;        //rise write enable
-        address_tb = 3'b111;  //REG7
-        wrData_tb = 'd9;     //write 9
-        #10
-        wrEN_tb = 1'b0;    //drop write enable 
-        #10
-        //read
-        rdEN_tb = 1'b1; //rise read enable
-        #10
-        address_tb = 3'b001;  //REG1
-        #10
-        if (rdData_tb == 'd13) count = count + 1;
-        #10
-        address_tb = 3'b101;  //REG5
-        #10
-        if (rdData_tb == 'd25) count = count + 1;
-        #10
-        address_tb = 3'b111;  //REG7
-        #10
-        if (rdData_tb == 'd9) count = count + 1;
-        #10
-        rdEN_tb =1'b0;//drop write
+            // 2nd scenario : Functional Correctness (Write on a register in a specified address)
+                $display("\n==================== 2nd scenario : Functional Correctness (Write on a register in a specified address) ====================");
+                reset();
+                wrEN = 1;           // enables write operation
+                address = 5;        // choose REG[5] to write in it 
+                wrData = 20;        // write 20 to REG[5]
+                @(negedge clk);     // waits for a clc cycle to track singals
+                if(DUT.REG[5] == 20) begin
+                    succeeded_cases = succeeded_cases + 1;      // another test case passes
+                    $display("[PASS] 2nd scenario : Functional Correctness (Write on a register in a specified address) | REG [5] = %d, expected = %d", 
+                            DUT.REG[5], wrData);
+                end else begin
+                    $display("[FAIL] 2nd scenario : Functional Correctness (Write on a register in a specified address) | REG [5] = %d, expected = %d", 
+                            DUT.REG[5], wrData);
+                end 
 
-        if (count == 3)begin
-            $display("TEST2 succeeded\n");
-            succeeded = succeeded + 1;
-        end 
-        else $display("TEST2 failed count is %d\n",count);
+            // 3rd scenario : Functional Correctness (Reset just after Write operation)
+                $display("\n==================== 3rd scenario : Functional Correctness (Reset just after Write operation) ====================");
+                reset();    
+                wrEN = 1;           // enables write operation
+                address = 10;       // choose REG[5] to write in it 
+                wrData = 16'h23;    // write hex(23) to REG[10]
+                @(negedge clk);     // waits for a clc cycle to track singals
+                reset();            // reset just after write operation to test bahavior
+                if(DUT.REG [10] == 0) begin
+                    succeeded_cases = succeeded_cases + 1;      // acother test case passes
+                    $display("[PASS] 3rd scenario : Functional Correctness (Reset just after Write operation) | REG [10] = %d, expected = %d", 
+                            DUT.REG [10], wrData);
+                end
+                else begin
+                    $display("[FAIL] 3rd scenario : Functional Correctness (Reset just after Write operation) | REG [10] = %d, expected = %d", 
+                            DUT.REG [10], wrData);
+                end
 
+            // 4th scenario : Corner Case (Write to the same REG 2 times [check overwrite])
+                $display("\n==================== 4th scenario : Corner Case (Write to the same REG 2 times [check overwrite]) ====================");
+                reset(); 
+                // first write
+                wrEN = 1;           // enables write operation
+                address = 0;        // choose REG[0] to write in it 
+                wrData = 16'h23;    // write hex(23) to REG[10]
+                @(negedge clk);     // waits for a clc cycle to track singals
+                // second write
+                wrEN = 1;           // enables write operation
+                address = 0;        // choose REG[0] to write in it 
+                wrData = 16'h11;    // write hex(11) to REG[0]
+                @(negedge clk);     // waits for a clc cycle to track singals
+                // check if it stores the new value
+                if(DUT.REG [0] == 16'h11) begin
+                    succeeded_cases = succeeded_cases + 1;      // acother test case passes
+                    $display("[PASS] 4th scenario : Corner Case (Write to the same REG 2 times [check overwrite]) | REG [0] = %d, expected = %d", 
+                            DUT.REG [0], wrData);
+                end
+                else begin
+                    $display("[FAIL] 34th scenario : Corner Case (Write to the same REG 2 times [check overwrite]) | REG [0] = %d, expected = %d", 
+                            DUT.REG [0], wrData);
+                end
 
-    //TEST3 Write data when active is not enabled
-        address_tb = 3'b000;  //REG0
-        wrData_tb = 'd98;    //write 98 
-        #10
-        rdEN_tb = 1'b1;    //rise read enable
-        #10
-        if (rdData_tb == 'd5)begin //the old stored value
-            $display("TEST3 succeeded\n");
-            succeeded = succeeded + 1;
-        end 
-        else $display("TEST3 failed\n");
-        rdEN_tb =1'b0;//drop write
-    
-    //TEST4 reset enabled
-        rst_tb = 1'b0;
-        #10
-        rst_tb = 1'b1;
-        #10
-        address_tb = 3'b000;  //REG0
-        #10
-        rdEN_tb = 1'b1;     //rise read enable
-        #10
-        if (rdData_tb == 'd0)begin //REG0 back to 0
-            $display("TEST4 succeeded\n");
-            succeeded = succeeded + 1;
-        end 
-        else $display("TEST4 failed\n");
-        rdEN_tb =1'b0;//drop write
+            // 5th scenario : Corner Case (Write FF to the last REG)
+                $display("\n==================== 5th scenario : Corner Case (Write FF to the last REG) ====================");
+                reset(); 
+                wrEN = 1;           // enables write operation
+                address = DEPTH-1;  // choose REG[DEPTH-1] to write in it 
+                wrData = 16'hFF;    // write hex(FF) to REG[DEPTH-1]
+                @(negedge clk);     // waits for a clc cycle to track singals
+                if(DUT.REG [DEPTH-1] == 16'hFF) begin
+                    succeeded_cases = succeeded_cases + 1;      // acother test case passes
+                    $display("[PASS] 5th scenario : Corner Case (Write FF to the last REG) | REG [%d-1] = %d, expected = %d", 
+                            DEPTH, DUT.REG [DEPTH-1], wrData);
+                end
+                else begin
+                    $display("[FAIL] 5th scenario : Corner Case (Write FF to the last REG) | REG [%d-1] = %d, expected = %d", 
+                            DEPTH, DUT.REG [DEPTH-1], wrData);
+                end
 
-    $display("TESTING ended %d out of %d succedded\n",succeeded,tests);    
-    $finish;
-end
-
-//monitor
-initial begin
-    $monitor("rst: %b ,wrEN: %b ,rdEN: %b ,address: %3b ,wrData: %d ,rdData: %d \n"
-                ,rst_tb ,wrEN_tb ,rdEN_tb ,address_tb ,wrData_tb ,rdData_tb );
-end
+            // 6th scenario : Corner Case (Write 00 to the last REG)
+                $display("\n==================== 6th scenario : Corner Case (Write 00 to the last REG) ====================");
+                reset(); 
+                wrEN = 1;           // enables write operation
+                address = DEPTH;    // choose REG[DEPTH] to write in it 
+                wrData = 16'h00;    // write hex(00) to REG[DEPTH]
+                @(negedge clk);     // waits for a clc cycle to track singals
+                if(DUT.REG [0] == 16'h00) begin
+                    succeeded_cases = succeeded_cases + 1;      // acother test case passes
+                    $display("[PASS] 6th scenario : Corner Case (Write 00 to the last REG) | REG [0] = %d, expected = %d", 
+                            DUT.REG [0], wrData);
+                end
+                else begin
+                    $display("[FAIL] 6th scenario : Corner Case (Write 00 to the last REG) | REG [0] = %d, expected = %d", 
+                            DUT.REG [0], wrData);
+                end
+            // STOP Simulation
+                $display("\n==================== Finish Simulation ====================");
+                $display("Succeeded Test Cases = %d\n All Tests = %d", succeeded_cases, all_cases);
+                #100;   // wait some time units
+                $stop;
+        end
 endmodule
